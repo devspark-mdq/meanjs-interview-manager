8baeed87645145108204cd784ff80079556dc472
Walter Zalazar <wzalazar@devspark.com>
2014-11-19 18:19:40 -0300

    add model and controller for technology

---
+++ app/controllers/technology.server.controller.js
@@ -0,0 +1,109 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	errorHandler = require('./errors.server.controller'),
+	Techonology = mongoose.model('Techonology'),
+	_ = require('lodash');
+
+/**
+ * Create a technology
+ */
+exports.create = function(req, res) {
+	var technology = new Techonology(req.body);
+	technology.user = req.user;
+
+	technology.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(technology);
+		}
+	});
+};
+
+/**
+ * Show the current technology
+ */
+exports.read = function(req, res) {
+	res.json(req.technology);
+};
+
+/**
+ * Update a technology
+ */
+exports.update = function(req, res) {
+	var technology = req.technology;
+
+	technology = _.extend(technology, req.body);
+
+	technology.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(technology);
+		}
+	});
+};
+
+/**
+ * Delete an technology
+ */
+exports.delete = function(req, res) {
+	var technology = req.technology;
+
+	technology.remove(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(technology);
+		}
+	});
+};
+
+/**
+ * List of Techonologys
+ */
+exports.list = function(req, res) {
+	Techonology.find().sort('-created').populate('user', 'displayName').exec(function(err, technologys) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.json(technologys);
+		}
+	});
+};
+
+/**
+ * Techonology middleware
+ */
+exports.technologyByID = function(req, res, next, id) {
+	Techonology.findById(id).populate('user', 'displayName').exec(function(err, technology) {
+		if (err) return next(err);
+		if (!technology) return next(new Error('Failed to load technology ' + id));
+		req.technology = technology;
+		next();
+	});
+};
+
+/**
+ * Techonology authorization middleware
+ */
+exports.hasAuthorization = function(req, res, next) {
+	if (req.technology.user.id !== req.user.id) {
+		return res.status(403).send({
+			message: 'User is not authorized'
+		});
+	}
+	next();
+};
---
+++ app/models/technology.server.model.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Question Schema
+ */
+
+var QuestionSchema = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+/**
+ * Technology Schema
+ */
+var TechnologySchema = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	name: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Name cannot be blank'
+	},
+	description: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	tags : {
+		type: [String]
+	},
+	questions: {
+		type: [QuestionSchema]
+	}
+});
+
+mongoose.model('Technology', TechnologySchema);
