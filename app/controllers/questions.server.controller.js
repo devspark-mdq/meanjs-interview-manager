6a109954c66e2fe07cc82c44b9dd764623eb862c
Patricio Gabriel Maseda <pmaseda@devspark.com>
2014-12-02 15:08:00 -0300

    Questions list fixed. Now getting questions from the technology array.

--- app/controllers/questions.server.controller.js
+++ app/controllers/questions.server.controller.js
@@ -93,15 +93,8 @@ exports.delete = function(req, res) {
  * List of Questions
  */
 exports.list = function(req, res) { 
-	Question.find().sort('-created').populate('user', 'displayName').exec(function(err, questions) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(questions);
-		}
-	});
+
+	res.jsonp(req.technology.questions);
 };
 
 /**
